1946ba4d42adfcc0b76feff29b021f835a4e3043
mengwanglalala <2623532708@qq.com>
2021-03-01 11:03:56 +0800

    first-commit

---
+++ CnnDQN.py
@@ -0,0 +1,191 @@
+import math, random
+import gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.autograd as autograd
+import torch.nn.functional as F
+from collections import  Counter
+from collections import deque
+import matplotlib.pyplot as plt
+from wrappers import make_atari, wrap_deepmind, wrap_pytorch
+
+USE_CUDA = torch.cuda.is_available()
+#将变量放到cuda上
+Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
+
+class CnnDQN(nn.Module):
+    def __init__(self, observation_space, action_sapce):
+        super(CnnDQN, self).__init__()
+
+        self.observation_space = observation_space
+        self.action_sapce = action_sapce
+
+        self.features = nn.Sequential(
+            nn.Conv2d(self.observation_space[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(7 * 7 * 64, 512),
+            nn.ReLU(),
+            nn.Linear(512,self.action_sapce)
+        )
+
+    def forward(self,x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)#将多维度的Tensor展平成一维
+        # x.size(0)指batchsize的值,x = x.view(x.size(0), -1)简化x = x.view(batchsize, -1),view()函数的功能根reshape类似，用来转换size大小。
+        # x = x.view(batchsize, -1)中batchsize指转换后有几行，而-1指在不告诉函数有多少列的情况下，根据原tensor数据和batchsize自动分配列数。
+        x = self.fc(x)
+        return x
+
+    # def feature_size(self):
+    #     #这里就很粗暴，先建立一个大小和预期输入的全0tensor，送入features中运行，最后得到输出，展平，读取长度。这里是7 * 7 * 64
+    #     return self.features(autograd.Variable(torch.zeros(1, *self.observation_space))).view(1, -1).size(1)
+
+    def act(self, state, epsilon):
+        if random.random() > epsilon:
+            state = Variable(torch.FloatTensor(np.float32(state)).unsqueeze(0), volatile=True)#(1,1,84,84)
+            q_value = self.forward(state)
+            action = q_value.max(1)[1].data[0]
+            action = action.cpu().numpy()  # 从网络中得到的tensor形式，因为之后要输入给gym环境中，这里把它放回cpu，转为数组形式
+            action = int(action)
+
+        else:
+            action = random.randrange(self.action_sapce)
+        return action
+
+class ReplayBuffer(object):
+    def __init__(self, capacity):
+        #deque模块是python标准库collections中的一项，它提供了两端都可以操作的序列，其实就是双向队列，
+        #可以从左右两端增加元素，或者是删除元素。如果设置了最大长度，非输入端的数据会逐步移出窗口。
+        self.buffer = deque (maxlen = capacity)
+
+    def push (self, state ,aciton, reward, next_state, done):
+        state = np.expand_dims(state,0)
+        #这里增加维度的操作是为了便于之后使用concatenate进行拼接
+        next_state = np.expand_dims(next_state,0)
+        self.buffer.append((state, aciton, reward, next_state, done))
+
+    def sample(self, batch_size):
+        # 将可迭代的对象作为参数，将对象中对应的元素打包成一个个元组，然后返回由这些元组组成的列表
+        state , action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
+        #最后使用concatenate对数组进行拼接，相当于少了一个维度
+        return np.concatenate(state),  action, reward, np.concatenate(next_state), done
+
+
+def compute_td_loss(model,optimizer, replay_buffer, gamma, batch_size):
+    state, action, reward, next_state, done = replay_buffer.sample(batch_size)
+    #通通丢到GPU上去
+    state = Variable(torch.FloatTensor(np.float32(state)))
+    next_state = Variable(torch.FloatTensor(np.float32(next_state)), volatile=True)
+    action = Variable(torch.LongTensor(action))
+    reward = Variable(torch.FloatTensor(reward))
+    done = Variable(torch.FloatTensor(done))
+
+    q_values = model(state)
+    next_q_values = model(next_state)
+
+    q_value = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
+    #gather可以看作是对q_values的查询，即元素都是q_values中的元素，查询索引都存在action中。输出大小与action.unsqueeze(1)一致。
+    #dim=1,它存放的都是第1维度的索引；dim=0，它存放的都是第0维度的索引；
+    #这里增加维度主要是为了方便gather操作，之后再删除该维度
+    next_q_value = next_q_values.max(1)[0]
+
+    expected_q_value = reward + gamma * next_q_value * (1 - done)
+
+    loss = (q_value - Variable(expected_q_value.data)).pow(2).mean()
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return loss
+
+
+def main():
+    env_id = "PongNoFrameskip-v4"
+    env = make_atari(env_id)
+    env = wrap_deepmind(env)
+    env = wrap_pytorch(env)
+
+    observation_space = env.observation_space.shape
+    action_sapce = env.action_space.n
+
+    model = CnnDQN(observation_space, action_sapce)
+
+    if USE_CUDA:
+        model = model.cuda()
+
+    optimizer = optim.Adam(model.parameters())
+
+    replay_buffer = ReplayBuffer(1000)
+
+    batch_size = 32
+    gamma = 0.99
+    replay_initial = 100
+    num_frames = 14000
+
+    losses = []
+    all_rewards = []
+    x_axis1 = []
+    x_axis2= []
+    episode_reward = 0
+
+    epsilon_start = 1.0
+    epsilon_final = 0.01
+    epsilon_decay = 30000
+
+    # 要求探索率随着迭代次数增加而减小
+    epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay)
+
+    state = env.reset()
+
+    for frame_idx in range(1, num_frames + 1):
+        #显示动画
+        env.render()
+        epsilon = epsilon_by_frame(frame_idx)
+        action = model.act(state, epsilon)
+        next_state, reward, done, _ = env.step(action)
+        replay_buffer.push(state, action, reward, next_state, done)
+        state = next_state
+        episode_reward += reward
+
+        if done:
+            state = env.reset()
+            x_axis1.append(frame_idx)
+            all_rewards.append(episode_reward)
+            episode_reward = 0
+
+        if frame_idx+1 > replay_initial:
+            loss = compute_td_loss(model, optimizer, replay_buffer, gamma, batch_size)
+            x_axis2.append(frame_idx)
+            losses.append(np.array(loss.data.cpu()))
+
+
+
+        if frame_idx % 100 == 0:
+            plt.figure(1)
+            plt.subplot(121)
+            plt.plot(x_axis1, all_rewards)
+            plt.subplot(122)
+            plt.plot(x_axis2, losses)
+            plt.show()
+
+    env.close()
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ DDPG.py
@@ -0,0 +1,260 @@
+import math
+import random
+import gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Normal
+import matplotlib.pyplot as plt
+
+use_cuda = torch.cuda.is_available()
+device   = torch.device("cuda" if use_cuda else "cpu")
+
+class ValueNetwork(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size ,init_w = 3e-3):
+        super(ValueNetwork, self).__init__()
+
+        self.linear1 = nn.Linear(num_inputs + num_actions, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, 1)
+
+        self.linear3.weight.data.uniform_(-init_w,init_w)
+        self.linear3.bias.data.uniform_(-init_w,init_w)
+
+    def forward(self, state, action):
+        x = torch.cat([state, action], 1)
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
+        return x
+
+class PolicyNetwork(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size, init_w = 3e-3):
+        super(PolicyNetwork, self).__init__()
+
+        self.linear1 = nn.Linear(num_inputs, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, num_actions)
+
+        # uniform_将tensor用从均匀分布中抽样得到的值填充。参数初始化
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        #也用用normal_(0, 0.1) 来初始化的，高斯分布中抽样填充，这两种都是比较有效的初始化方式
+        self.linear3.bias.data.uniform_(-init_w, init_w)
+        #其意义在于我们尽可能保持 每个神经元的输入和输出的方差一致。
+        #使用 RELU（without BN） 激活函数时，最好选用 He 初始化方法，将参数初始化为服从高斯分布或者均匀分布的较小随机数
+        #使用 BN 时，减少了网络对参数初始值尺度的依赖，此时使用较小的标准差(eg：0.01)进行初始化即可
+
+        #但是注意DRL中不建议使用BN
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = F.tanh(self.linear3(x))
+        return x
+
+    def get_action(self, state):
+        state = torch.FloatTensor(state).unsqueeze(0).to(device)
+        action = self.forward(state)
+        return action.detach().cpu().numpy()[0,0]
+
+class OUNoise(object):
+    def __init__(self, action_space, mu=0.0, theta = 0.15, max_sigma = 0.3, min_sigma = 0.3, decay_period = 100000):#decay_period要根据迭代次数合理设置
+        self.mu = mu
+        self.theta = theta
+        self.sigma = max_sigma
+        self.max_sigma = max_sigma
+        self.min_sigma = min_sigma
+        self.decay_period = decay_period
+        self.action_dim = action_space.shape[0]
+        self.low = action_space.low
+        self.high = action_space.high
+        self.reset()
+
+    def reset(self):
+        self.state = np.ones(self.action_dim) *self.mu
+
+    def evolve_state(self):
+        x = self.state
+        dx = self.theta* (self.mu - x) + self.sigma * np.random.randn(self.action_dim)
+        self.state = x + dx
+        return self.state
+
+    def get_action(self, action, t=0):
+        ou_state = self.evolve_state()
+        self.sigma = self.max_sigma - (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
+        return np.clip(action + ou_state, self.low, self.high)
+
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.buffer = []
+        self.position = 0
+
+    def push(self, state, action, reward, next_state, done):
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(None)
+        self.buffer[self.position] = (state, action, reward, next_state, done)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        state, action, reward, next_state, done = map(np.stack, zip(*batch))
+        return state, action, reward, next_state, done
+
+    def __len__(self):
+        return len(self.buffer)
+
+
+class NormalizedActions(gym.ActionWrapper):
+
+    def action(self, action):
+        low_bound = self.action_space.low
+        upper_bound = self.action_space.high
+
+        action = low_bound + (action + 1.0) * 0.5 * (upper_bound - low_bound)
+        #将经过tanh输出的值重新映射回环境的真实值内
+        action = np.clip(action, low_bound, upper_bound)
+
+        return action
+
+    def reverse_action(self, action):
+        low_bound = self.action_space.low
+        upper_bound = self.action_space.high
+
+        #因为激活函数使用的是tanh，这里将环境输出的动作正则化到（-1，1）
+
+        action = 2 * (action - low_bound) / (upper_bound - low_bound) - 1
+        action = np.clip(action, low_bound, upper_bound)
+
+        return action
+
+class DDPG(object):
+    def __init__(self, action_dim, state_dim, hidden_dim):
+        super(DDPG,self).__init__()
+        self.action_dim, self.state_dim, self.hidden_dim = action_dim, state_dim, hidden_dim
+        self.batch_size = 128
+        self.gamma = 0.99
+        self.min_value = -np.inf
+        self.max_value = np.inf
+        self.soft_tau = 1e-2
+        self.replay_buffer_size = 5000
+        self.value_lr = 1e-3
+        self.policy_lr = 1e-4
+
+        self.value_net = ValueNetwork(state_dim, action_dim, hidden_dim).to(device)
+        self.policy_net = PolicyNetwork(state_dim, action_dim, hidden_dim).to(device)
+
+        self.target_value_net = ValueNetwork(state_dim, action_dim, hidden_dim).to(device)
+        self.target_policy_net = PolicyNetwork(state_dim, action_dim, hidden_dim).to(device)
+
+        for target_param, param in zip(self.target_value_net.parameters(), self.value_net.parameters()):
+            target_param.data.copy_(param.data)
+
+        for target_param, param in zip(self.target_policy_net.parameters(), self.policy_net.parameters()):
+            target_param.data.copy_(param.data)
+
+        self.value_optimizer = optim.Adam(self.value_net.parameters(), lr=self.value_lr)
+        self.policy_optimizer = optim.Adam(self.policy_net.parameters(), lr=self.policy_lr)
+
+        self.value_criterion = nn.MSELoss()
+
+        self.replay_buffer = ReplayBuffer(self.replay_buffer_size)
+
+    def ddpg_update(self):
+        state, action, reward, next_state, done = self.replay_buffer.sample(self.batch_size)
+
+        state = torch.FloatTensor(state).to(device)
+        next_state = torch.FloatTensor(next_state).to(device)
+        action = torch.FloatTensor(action).to(device)
+        reward = torch.FloatTensor(reward).unsqueeze(1).to(device)
+        done = torch.FloatTensor(np.float32(done)).unsqueeze(1).to(device)
+
+        policy_loss = self.value_net(state, self.policy_net(state))
+        policy_loss = -policy_loss.mean()
+
+        next_action = self.target_policy_net(next_state)
+        target_value = self.target_value_net(next_state, next_action.detach())
+        expected_value = reward + (1.0 - done) * self.gamma * target_value
+        expected_value = torch.clamp(expected_value, self.min_value, self.max_value)
+
+        value = self.value_net(state, action)
+        value_loss = self.value_criterion(value, expected_value.detach())
+
+        self.policy_optimizer.zero_grad()
+        policy_loss.backward()
+        self.policy_optimizer.step()
+
+        self.value_optimizer.zero_grad()
+        value_loss.backward()
+        self.value_optimizer.step()
+
+        for target_param, param in zip(self.target_value_net.parameters(), self.value_net.parameters()):
+            target_param.data.copy_(
+                target_param.data * (1.0 - self.soft_tau) + param.data * self.soft_tau
+            )
+
+        for target_param, param in zip(self.target_policy_net.parameters(), self.policy_net.parameters()):
+            target_param.data.copy_(
+                target_param.data * (1.0 - self.soft_tau) + param.data * self.soft_tau
+            )
+
+def plot(frame_idx, rewards):
+    plt.figure(figsize=(20,5))
+    plt.subplot(131)
+    plt.title('frame %s. reward: %s' % (frame_idx, rewards[-1]))
+    plt.plot(rewards)
+    plt.show()
+
+
+def main():
+    env = gym.make("Pendulum-v0")
+    env = NormalizedActions(env)
+
+    ou_noise = OUNoise(env.action_space)
+
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.shape[0]
+    hidden_dim = 256
+
+
+    ddpg = DDPG(action_dim, state_dim, hidden_dim)
+
+    max_frames = 12000
+    max_steps = 500
+    frame_idx = 0
+    rewards = []
+    batch_size = 128
+
+    while frame_idx < max_frames:
+        state = env.reset()
+        ou_noise.reset()
+        episode_reward = 0
+
+        for step in range(max_steps):
+            env.render()
+            action = ddpg.policy_net.get_action(state)
+            action = ou_noise.get_action(action, step)
+            next_state, reward, done, _ = env.step(action)
+
+            ddpg.replay_buffer.push(state, action, reward, next_state, done)
+            if len(ddpg.replay_buffer) > batch_size:
+                ddpg.ddpg_update()
+
+            state = next_state
+            episode_reward += reward
+            frame_idx += 1
+
+            if frame_idx % max(1000, max_steps + 1) == 0:
+                plot(frame_idx, rewards)
+
+            if done:
+                break
+
+        rewards.append(episode_reward)
+    env.close()
+
+if __name__ == '__main__':
+    main()
